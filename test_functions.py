0d42e406607839f47f992d20a0f99ed594dfa626
mops26 <ol.morozoff2@gmail.com>
2026-03-14 23:03:33 +0300

    Add files via upload

---
+++ test_functions.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def f1_poly5(x):
+    return x**5 - 5*x**3 + 4*x + 1
+
+def f2_sin_neg2(x):
+    return 1 / (np.sin(x)**2)
+
+def f3_exp_decay(x):
+    return np.exp(-x**2)
+
+def f4_step(x):
+    return np.where(x < 1.5, 1.0, 2.0)
+
+
+def exact_values():
+    return {
+        'f1': 2**6/6 - 5*2**4/4 + 4*2**2/2 + 2,
+        'f2': 1/np.tan(0.5) - 1/np.tan(2),
+        'f3': 0.886226925452758,
+        'f4': 1*1.5 + 2*1.5
+    }
